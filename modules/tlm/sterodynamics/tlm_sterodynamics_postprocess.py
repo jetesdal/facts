e2a23d32f726b6e55550b8cd60bea5bbbf912104
Jan-Erik Tesdal <jt7058@princeton.edu>
2024-11-03 20:58:34 -0500

    Use the unadjusted TAS and ZOSTOGA when no corr and no subset

--- modules/tlm/sterodynamics/tlm_sterodynamics_postprocess.py
+++ modules/tlm/sterodynamics/tlm_sterodynamics_postprocess.py
@@ -260,8 +260,8 @@ def tlm_postprocess_oceandynamics(nsamps, rng_seed, chunksize, keep_temp, pipeli
 	if no_correlation and not subset_overlap:
 		# Save to xarray dataset with separate coordinates for model_zos and model_zostoga
 		ds_out = xr.Dataset({'zos': (['year', 'model_zos', 'location'], interim_data['sZOS']),
-				     'zostoga': (['year', 'model_zostoga'], interim_data['sZOSTOGAadj']),
-				     'tas': (['year', 'model_tas'], interim_data['sTASadj'])},
+				     'zostoga': (['year', 'model_zostoga'], interim_data['sZOSTOGA']),
+				     'tas': (['year', 'model_tas'], interim_data['sTAS'])},
 				    coords={'year': interim_data['zosyears'],
 					    'model_zos': interim_data['zos_modellist'],
 					    'model_zostoga': interim_data['zostoga_modellist'],
