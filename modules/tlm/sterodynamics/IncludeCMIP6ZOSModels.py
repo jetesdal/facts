c511b20795fd555507cc209358c0190409ef916e
Jan-Erik Tesdal <jt7058@princeton.edu>
2024-11-05 16:50:29 -0500

    Add compatibility checks to match historical and scenario runs
    Adds more filtering of historical and scenario files based on matching temporal frequency and ensemble member.

--- modules/tlm/sterodynamics/IncludeCMIP6ZOSModels.py
+++ modules/tlm/sterodynamics/IncludeCMIP6ZOSModels.py
@@ -114,14 +114,15 @@ def IncludeCMIP6ZOSModels(model_dir, varname, years, include_models, include_sce
 		if model not in os.listdir(model_dir):
 			continue
 
-		# initialize lists to store data
-		runtype_data = {'historical':[],scenario:[]}
-		runtype_datayrs = {'historical':[],scenario:[]}
+		# Initialize lists to store data
+		runtype_data = {'historical': [], scenario: []}
+		runtype_datayrs = {'historical': [], scenario: []}
+		runtype_fnames = {'historical': [], scenario: []}
 
 		incorporate = True # incorporate model or not
 
 		# Read in historical and ssp data
-		for runtype in ('historical',scenario):
+		for runtype in (scenario, 'historical'):
 
 			#start of filename for runtype currently processed
 			#filename_id = varname + '_Omon_' + model + '_' + runtype
@@ -131,8 +132,9 @@ def IncludeCMIP6ZOSModels(model_dir, varname, years, include_models, include_sce
 			for files_forModel in os.listdir(os.path.join(model_dir,model)): # loop through files in model folder
 				if (varname + '_Omon_' + model + '_' + runtype) in files_forModel or (varname + '_Oyr_' + model + '_' + runtype) in files_forModel:
 				#if files_forModel[0:len(filename_id)] == filename_id:
-					filename = files_forModel # assign filename
-					break
+					if not any(np.any(runtype_data[item]) for item in runtype_data) or (files_forModel.split('_')[1] in runtype_fnames[scenario] and files_forModel.split('_')[4] in runtype_fnames[scenario]):
+						filename = files_forModel # assign filename
+						break	
 
 			if not filename: #if the right filename cannot be found:
 				incorporate=False
@@ -187,6 +189,7 @@ def IncludeCMIP6ZOSModels(model_dir, varname, years, include_models, include_sce
 				#store into dict for each cmip6 runtype
 				runtype_data[runtype] = np.array(dat).T
 				runtype_datayrs[runtype] = np.array(datayrs)
+				runtype_fnames[runtype] = filename
 				#runtype_datayrs[runtype] = np.array(datayrs[::12]) # moved this to monthly to annual conversion
 				#runtype_data[runtype] = np.ma.array(dat).T
 				#runtype_datayrs[runtype] = np.ma.array(datayrs[::12])
