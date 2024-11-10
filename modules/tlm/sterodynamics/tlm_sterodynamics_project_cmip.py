1f009e367a8d03ea32d0a7511fd8ebd3398df088
Jan-Erik Tesdal <jt7058@princeton.edu>
2024-11-10 17:00:58 -0500

    Comment out additional randomization
    The commented out lines should not be necessary when specifying a seed for the random number generator that is different from the seed used in the DSL projection.

--- modules/tlm/sterodynamics/tlm_sterodynamics_project_cmip.py
+++ modules/tlm/sterodynamics/tlm_sterodynamics_project_cmip.py
@@ -99,9 +99,9 @@ def tlm_project_cmip(nsamps, seed, pipeline_id,scenario,pyear_start,pyear_end,py
 		temp = t.ppf(norm.cdf(norm_inv_perm), ThermExpDOF[this_year_ind])
 		#temp = t.ppf(norm.cdf(norm_inv_perm), ThermExpDOF[i])  # Replicates bug in K14 master code
 		
-		# Randomly sample indices
-		temp_idx = rng.choice(np.arange(len(temp)), nsamps)
-		temp = temp[temp_idx]
+		# Randomly sample indices (used when seed is not specified and ends up being the same as for DSL)
+		#temp_idx = rng.choice(np.arange(len(temp)), nsamps)
+		#temp = temp[temp_idx]
 		
 		thermsamps[:,i] = (ThermExpScale * temp * ThermExpStd[this_year_ind]) + ThermExpMean[this_year_ind]
 
