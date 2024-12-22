957160831aa849291f9b1b77113b1daa6c3b6c59
Jan-Erik Tesdal <jt7058@princeton.edu>
2024-12-22 13:52:06 -0500

    Comment out randomization
    Since we now ensure that the seed is different from the postprocess step, we can skip of randomization.

--- modules/kopp14/sterodynamics/kopp14_oceandynamics_project.py
+++ modules/kopp14/sterodynamics/kopp14_oceandynamics_project.py
@@ -84,8 +84,8 @@ def kopp14_project_oceandynamics(nsamps, seed, pipeline_id):
 		#temp = t.ppf(norm.cdf(norm_inv_perm), ThermExpN[i]-1)  # Replicates bug in K14 master code
 		
 		# Randomly sample indices
-		temp_idx = rng.choice(np.arange(len(temp)), nsamps)
-		temp = temp[temp_idx]
+		#temp_idx = rng.choice(np.arange(len(temp)), nsamps)
+		#temp = temp[temp_idx]
 		
 		thermsamps[:,i] = (ThermExpScale * temp * ThermExpStd[this_year_ind]) + ThermExpMean[this_year_ind]
 
